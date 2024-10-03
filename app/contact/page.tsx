66c311002f1451d771d2f64c3d752f74ee50eecd
gazni-experion <mohammed.gasni@experionglobal.com>
2024-10-03 21:27:18 +0530

    add date field in the contact us form

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,191 +1,164 @@
 'use client';
-import React, { useState } from 'react'
+import {
+    Button,
+    FormControlLabel,
+    FormHelperText,
+    InputLabel,
+    MenuItem,
+    TextField,
+    FormControl,
+    FormLabel,
+    RadioGroup,
+    Radio
+} from '@mui/material';
+import * as yup from 'yup';
+import { useFormik } from 'formik';
 
 
 export default function Contact() {
-    // States for contact form fields
-    const [fullname, setFullname] = useState("");
-    const [email, setEmail] = useState("");
-    const [subject, setSubject] = useState("");
-    const [message, setMessage] = useState("");
 
-    //   Form validation state
-    const [errors, setErrors] = useState({});
+    const weddingType = ['Hindu','Muslim','Christian','Non Religious'];
 
-    //   Setting button text on form submission
-    const [buttonText, setButtonText] = useState("Send");
+    const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
 
-    // Setting success or failure messages states
-    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-    const [showFailureMessage, setShowFailureMessage] = useState(false);
 
-    // Validation check method
-    const handleValidation = () => {
-        let tempErrors = {};
-        let isValid = true;
+    const validationSchema = yup.object({
+        name: yup.string().required('Please provide your name!'),
+        number: yup
+            .string()
+            .matches(phoneRegExp, 'Phone number is not valid!')
+            .min(10, "Please check the entered phone number!")
+            .max(13, "Please check the entered phone number!")
+            .required('Please enter your phone number!'),
+        email: yup
+            .string()
+            .email('Enter a valid email id'),
+        date: yup.date()
+            .required("Please provide the expected event date")
+    });
 
-        if (fullname.length <= 0) {
-            tempErrors["fullname"] = true;
-            isValid = false;
-        }
-        if (email.length <= 0) {
-            tempErrors["email"] = true;
-            isValid = false;
-        }
-        if (subject.length <= 0) {
-            tempErrors["subject"] = true;
-            isValid = false;
-        }
-        if (message.length <= 0) {
-            tempErrors["message"] = true;
-            isValid = false;
-        }
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            number: '',
+            email: '',
+            type: '',
+            date: '',
+            destination: '',
+            whatsapp: true,
+        },
 
-        setErrors({ ...tempErrors });
-        console.log("errors", errors);
-        return isValid;
-    };
-
-    //   Handling form submit
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-
-        let isValidForm = handleValidation();
-
-        if (isValidForm) {
-            setButtonText("Sending");
-            const body = JSON.stringify({
-                email: email,
-                fullname: fullname,
-                subject: subject,
-                message: message,
-            });
-            console.log(body);
-            
-            // const res = await fetch("/api/sendgrid", {
-            //     body: JSON.stringify({
-            //         email: email,
-            //         fullname: fullname,
-            //         subject: subject,
-            //         message: message,
-            //     }),
-            //     headers: {
-            //         "Content-Type": "application/json",
-            //     },
-            //     method: "POST",
-            // });
-
-            // const { error } = await res.json();
-            // if (error) {
-            //     console.log(error);
-            //     setShowSuccessMessage(false);
-            //     setShowFailureMessage(true);
-            //     setButtonText("Send");
-            //     return;
-            // }
-            setShowSuccessMessage(true);
-            setShowFailureMessage(false);
-            setButtonText("Send");
-        }
-        console.log(fullname, email, subject, message);
-    };
+        validationSchema,
+        onSubmit: (values) => {
+            alert(JSON.stringify(values, null, 2));
+        },
+    });
 
     return (
-        <form
-            onSubmit={handleSubmit}
-            className="rounded-lg shadow-xl flex flex-col px-8 py-8"
-        >
+        <form onSubmit={formik.handleSubmit} className="rounded-lg shadow-xl flex flex-col px-8 py-8">
             <h1 className="text-2xl font-bold dark:text-gray-50">
                 Get in touch with us
             </h1>
-
-            <label
-                htmlFor="fullname"
-                className="text-gray-500 font-light mt-8 dark:text-gray-50"
-            >
-                What should we call you ?<span className="text-red-500 dark:text-gray-50">*</span>
-            </label>
-            <input
-                type="text"
-                value={fullname}
-                onChange={(e) => {
-                    setFullname(e.target.value);
-                }}
-                name="fullname"
-                className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md text-gray-500"
-            />
-
-
-            <label
-                htmlFor="email"
-                className="text-gray-500 font-light mt-4 dark:text-gray-50"
-            >
-                E-mail<span className="text-red-500">*</span>
-            </label>
-            <input
-                type="email"
-                name="email"
-                value={email}
-                onChange={(e) => {
-                    setEmail(e.target.value);
-                }}
-                className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md font-light text-gray-500"
-            />
-
-
-            <label
-                htmlFor="subject"
-                className="text-gray-500 font-light mt-4 dark:text-gray-50"
-            >
-                Subject<span className="text-red-500">*</span>
-            </label>
-            <input
-                type="text"
-                name="subject"
-                value={subject}
-                onChange={(e) => {
-                    setSubject(e.target.value);
-                }}
-                className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md font-light text-gray-500"
-            />
-
-            <label
-                htmlFor="message"
-                className="text-gray-500 font-light mt-4 dark:text-gray-50"
-            >
-                Message<span className="text-red-500">*</span>
-            </label>
-            <textarea
-                name="message"
-                value={message}
-                onChange={(e) => {
-                    setMessage(e.target.value);
-                }}
-                className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md font-light text-gray-500"
-            ></textarea>
-
-            <div className="flex flex-row items-center justify-start">
-                <button
-                    type="submit"
-                    className="px-10 mt-8 py-2 bg-[#130F49] text-gray-50 font-light rounded-md text-lg flex flex-row items-center"
-                >
-                    Submit
-                    <svg
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        className="text-cyan-500 ml-2"
-                        fill="currentColor"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                            d="M9.00967 5.12761H11.0097C12.1142 5.12761 13.468 5.89682 14.0335 6.8457L16.5089 11H21.0097C21.562 11 22.0097 11.4477 22.0097 12C22.0097 12.5523 21.562 13 21.0097 13H16.4138L13.9383 17.1543C13.3729 18.1032 12.0191 18.8724 10.9145 18.8724H8.91454L12.4138 13H5.42485L3.99036 15.4529H1.99036L4.00967 12L4.00967 11.967L2.00967 8.54712H4.00967L5.44417 11H12.5089L9.00967 5.12761Z"
-                            fill="currentColor"
-                        />
-                    </svg>
-                </button>
+            <div>
+                <InputLabel>Your Name</InputLabel>
+                <TextField
+                    variant="standard"
+                    type='text'
+                    name='name'
+                    placeholder='What should we call you?'
+                    value={formik.values.name}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    error={formik.touched.name && Boolean(formik.errors.name)}
+                    helperText={formik.touched.name && formik.errors.name}
+                />
             </div>
+            <div>
+                <FormControl>
+                    <FormLabel>Preferred mode of communication</FormLabel>
+                    <RadioGroup
+                        defaultValue="true"
+                        name="whatsapp"
+                        row
+                        onChange={formik.handleChange}
+                    >
+                        <FormControlLabel value="true" control={<Radio />} label="WhatsApp" />
+                        <FormControlLabel value="false" control={<Radio />} label="Call" />
+                    </RadioGroup>
+                </FormControl>
+            </div>
+            <div>
+                <InputLabel>Contact Number</InputLabel>
+                <TextField
+                    variant="standard"
+                    type='text'
+                    name='number'
+                    placeholder='+91 xxx xxx xxxx'
+                    value={formik.values.number}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    error={formik.touched.number && Boolean(formik.errors.number)}
+                    helperText={formik.touched.number && formik.errors.number}
+                />
+            </div>
+            <div>
+                <InputLabel>Email Address</InputLabel>
+                <TextField
+                    variant="standard"
+                    type='text'
+                    name='email'
+                    placeholder='Email Address'
+                    value={formik.values.email}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    error={formik.touched.email && Boolean(formik.errors.email)}
+                    helperText={formik.touched.email && formik.errors.email}
+                />
+            </div>
+            <div>
+                <InputLabel>Wedding Type</InputLabel>
+                <TextField
+                    variant="standard"
+                    select
+                    defaultValue='Hindu'
+                    name='type'
+                    value={formik.values.type}
+                    onChange={formik.handleChange}
+                >
+                    {weddingType.map((option) => (
+                        <MenuItem key={option} value={option}>
+                            {option}
+                        </MenuItem>
+                    ))}
+                </TextField>
+            </div>
+            <div>
+                <InputLabel>Destination</InputLabel>
+                <TextField
+                    variant="standard"
+                    type='text'
+                    name='destination'
+                    placeholder='Wedding destination'
+                    value={formik.values.destination}
+                    onChange={formik.handleChange}
+                />
+            </div>
+            <div>
+                <InputLabel>Wedding Date</InputLabel>
+                <TextField
+                    variant="standard"
+                    type="date"
+                    name='date'
+                    value={formik.values.date}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    error={formik.touched.date && Boolean(formik.errors.date)}
+                    helperText={formik.touched.date && formik.errors.date}
+                />
+            </div>
+            <Button type='submit' variant='contained' sx={{ mt: '15px' }}>
+                Submit
+            </Button>
         </form>
-    )
+    );
 }
