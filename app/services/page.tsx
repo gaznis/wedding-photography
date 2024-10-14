313258fea68947635b2be4fbbd1835b78329a5e6
gazni-experion <mohammed.gasni@experionglobal.com>
2024-10-14 10:20:49 +0530

    Add services page

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,8 +1,53 @@
-// `app/dashboard/page.tsx` is the UI for the `/dashboard` URL
-export default function Page() {
+import { Card, Image, Row, Col, Descriptions } from 'antd';
+import { SERVICES } from '@/constants/services'
+
+
+export default function Page() {    
     return (
-            <div className="flex flex-col gap-8 row-start-2 items-center justify-center m-3">
-            <h1>Hellooo.... Look in to our services</h1>
-            </div>
+        <div className="overflow-x-auto md:overflow-x-hidden pb-4">
+
+            <Row
+                align="stretch"
+                gutter={[16, 24]}
+                className="flex flex-nowrap sm:flex-wrap"
+                style={{ minWidth: 'min-content' }}
+            >
+            {SERVICES.map((service) => (
+                <Col
+                    sm={{
+                        span:24,
+                    }}
+                    md={{
+                        span:4
+                    }}
+                    offset={1} key={service.id}>
+                    <Card
+                        bordered={false}
+                        size='small'
+            hoverable
+                    style={{
+                        width: 240,
+             }}
+                        cover={<Image preview={false} alt={service.alt} src={service.src} style={{ height: 240, alignItems: 'center', overflow: 'hidden', objectFit: 'scale-down' }}/>}
+                >
+                        <Descriptions title={service.title} size='small'>
+                            <p>
+                            {service.description}
+                            </p>
+                    </Descriptions>
+                        <div className='grid'>
+                            <strong>
+
+                        <p className='text-gray-500'>starting at</p>
+                            <p> &#8377; {service.price}</p>
+                            </strong>
+                    </div>
+        </Card>
+            </Col>
+                )
+                )}
+        </Row>
+        </div>
+
     );
 }
---
+++ constants/services.ts
@@ -0,0 +1,42 @@
+export const SERVICES = [
+    {
+    id:0,
+    title: "Engagement",
+    description: "Every glance, every smile, every whispered promise—these are the moments that tell your love story. Let’s capture the magic of your engagement, framing the beginning of forever.",
+    price: "35,000",
+    alt: "engagement",
+    src:"cards/lady1.jpg"
+},
+    {
+    id:1,
+    title: "Pre / Post Wedding",
+        description: "Amidst the calm before promises or the glow that follows, take a day, a place, and let your love unfold. In the laughter, in the quiet, we’ll capture your story—untouched by the rush, pure and timeless. Before the big day’s rush, let us freeze your calm and carefree moments in time—with a pre or post-wedding shoot that tells the beautiful story of just you two.",
+    price: "20,000",
+    alt: "pre wedding",
+    src:"cards/lady2.jpg"
+},
+    {
+    id:2,
+    title: "Haldi",
+    description: "In hues of golden sunshine and laughter’s warm embrace, the Haldi ceremony blooms—a ritual of joy, a blessing in yellow. Let us capture the glow of tradition, the laughter shared, and the beauty of love painted across every smile. Moments to cherish, wrapped in turmeric and sunlight, marking the start of forever.",
+    price: "10,000",
+    alt: "haldi",
+    src:"cards/lady3.jpg"
+},
+    {
+    id:3,
+    title: "Wedding",
+        description: "As two souls intertwine beneath a canopy of dreams, a wedding unfolds—an exquisite tapestry woven with love, laughter, and cherished vows. Let us capture the magic of this day, the glimmer in your eyes, the joyful tears, and the dance of hearts uniting. Every moment, a precious thread in the story of forever.",
+    price: "65,000",
+    alt: "wedding",
+    src:"cards/lady1.jpg"
+},
+    {
+    id:4,
+    title: "Baptism / Birthday",
+    description: "In the joyful embrace of love and laughter, we come together to celebrate the gift of life—a baptism, a birthday, a cherished milestone! Let us capture the twinkle in those tiny eyes, the giggles of family, and the warmth of hearts united. Each photograph will be a joyful chapter in their beautiful journey, filled with love and laughter.",
+    price: "35,000",
+    alt: "baptism",
+    src:"cards/lady2.jpg"
+},
+];
